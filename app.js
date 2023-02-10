2bcd3fa329b8fa96e88a3931ed2319db05453d51
joseph_dev <joseph.schneider0105@gmail.com>
2023-02-09 19:43:54 -0500

    L'affichage du cookie se fait, mais en cliquant sur créer, de plus il affiche undifined sur l'écran. Mais le cookie est bien crée en appli

--- app.js
+++ app.js
@@ -13,8 +13,26 @@ function handleSubmit(event) {
   const cookieData = inputName.value + inputValue.value;
 
   creatCookie();
+  displayCookie();
 }
 
 function creatCookie(cookieData) {
   document.cookie = `${inputName.value}=${inputValue.value}`;
+}
+
+function displayCookie(cookieData) {
+  const card = document.createElement("div");
+  card.innerHTML = `
+  <div class="display-card">
+    <div class="result-name">
+        <p>Son nom: ${cookieData}</p>
+    </div>
+    <div class="result-value">
+        <p>Sa valeur: ${cookieData}</p>
+    </div>
+    <div class="display-cross">x</div>
+      
+    </div>`;
+  displayCookies.style.display = "block";
+  displayCookies.appendChild(card);
 }
