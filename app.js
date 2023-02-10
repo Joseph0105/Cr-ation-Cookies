6b18b76283828abd169b0b6ad7a54e35ba7a6b48
joseph_dev <joseph.schneider0105@gmail.com>
2023-02-10 16:29:59 -0500

    Les cookies s'affichent correctement après l'appel de displayCookie, mais seulement un s'affiche pour l'instant

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const buttonCreat = document.querySelector(".button-creat");
 const buttonDisplay = document.querySelector(".button-display");
 const inputName = document.querySelector(".input-name");
 const inputValue = document.querySelector(".input-value");
+let name, value;
 
 buttonCreat.addEventListener("click", handleSubmit);
 buttonDisplay.addEventListener("click", handleDisplay);
@@ -17,27 +18,43 @@ function handleSubmit(event) {
   creatCookie();
 }
 
-function creatCookie(cookieData) {
-  document.cookie = `${inputName.value}=${inputValue.value}`;
+function creatCookie(name, value) {
+  // const [name, value] = cookieData.split("=");
+  nameOf = inputName.value;
+  valueOf = inputValue.value;
+  document.cookie = `${nameOf}=${valueOf}`;
+  // document.cookie = `${inputName.value}=${inputValue.value}`;
 }
 
 function handleDisplay(e) {
   e.preventDefault();
+  // const cookieData = inputName.value + inputValue.value;
   displayCookie();
 }
-function displayCookie(cookieData) {
+
+async function displayCookie() {
+  let name, value;
+  const cookies = document.cookie.split(";");
+
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].split("=");
+    name = cookie[0].trim();
+    value = cookie[1].trim();
+  }
+
   const card = document.createElement("div");
   card.innerHTML = `
   <div class="display-card">
     <div class="result-name">
-        <p>Son nom: ${cookieData}</p>
+        <p>Son nom: ${name}</p>
     </div>
     <div class="result-value">
-        <p>Sa valeur: ${cookieData}</p>
+        <p>Sa valeur: ${value}</p>
     </div>
     <div class="display-cross">x</div>
       
     </div>`;
+  console.log(name);
   displayCookies.style.display = "block";
   displayCookies.appendChild(card);
 }
