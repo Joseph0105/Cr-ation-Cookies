85bcc35a010b72cc691669e81acc1e1098dd373b
joseph_dev <joseph.schneider0105@gmail.com>
2023-02-13 17:58:41 -0500

    les cookies s'affichent correctement, maintenant il faut que le message d'erreur s'affiche si il n'yen a pas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("form");
 const displayCookies = document.querySelector(".display-result");
 const errorMsg = document.querySelector(".error-msg");
+const errorDisplay = document.querySelector(".error-display");
 
 const buttonCreat = document.querySelector(".button-creat");
 const buttonDisplay = document.querySelector(".button-display");
@@ -8,53 +9,66 @@ const inputName = document.querySelector(".input-name");
 const inputValue = document.querySelector(".input-value");
 let name, value;
 
+let cookiesData = [];
+
 buttonCreat.addEventListener("click", handleSubmit);
 buttonDisplay.addEventListener("click", handleDisplay);
 
 function handleSubmit(event) {
   event.preventDefault();
-  const cookieData = inputName.value + inputValue.value;
+  const cookieData = {
+    name: inputName.value,
+    value: inputValue.value,
+  };
+  cookiesData.push(cookieData);
 
-  creatCookie();
+  creatCookie(cookieData.name, cookieData.value);
 }
 
 function creatCookie(name, value) {
-  // const [name, value] = cookieData.split("=");
-  nameOf = inputName.value;
-  valueOf = inputValue.value;
-  document.cookie = `${nameOf}=${valueOf}`;
-  // document.cookie = `${inputName.value}=${inputValue.value}`;
+  document.cookie = `${name}=${value}`;
 }
 
-function handleDisplay(e) {
-  e.preventDefault();
-  // const cookieData = inputName.value + inputValue.value;
-  displayCookie();
+function getCookies() {
+  return document.cookie.split("; ").map((cookie) => {
+    const [name, value] = cookie.split("=");
+    return { name, value };
+  });
 }
 
-async function displayCookie() {
-  let name, value;
-  const cookies = document.cookie.split(";");
-
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].split("=");
-    name = cookie[0].trim();
-    value = cookie[1].trim();
-  }
-
-  const card = document.createElement("div");
-  card.innerHTML = `
+function displayCookie() {
+  displayCookies.style.display = "block";
+  const cookies = getCookies();
+  for (const cookieData of cookies) {
+    const card = document.createElement("div");
+    card.innerHTML = `
   <div class="display-card">
     <div class="result-name">
-        <p>Son nom: ${name}</p>
+        <p>Son nom: ${cookieData.name}</p>
     </div>
     <div class="result-value">
-        <p>Sa valeur: ${value}</p>
+        <p>Sa valeur: ${cookieData.value}</p>
     </div>
     <div class="display-cross">x</div>
       
     </div>`;
-  console.log(name);
-  displayCookies.style.display = "block";
-  displayCookies.appendChild(card);
+    console.log(cookiesData);
+
+    displayCookies.appendChild(card);
+  }
+}
+
+function displayError(cookies) {
+  if (cookies.length === 0) {
+    errorDisplay.textContent = "Aucuns cookies n'a été crée sur ce site.";
+  } else {
+    errorDisplay.textContent = "";
+  }
+}
+
+async function handleDisplay(e) {
+  e.preventDefault();
+  const cookies = getCookies();
+  displayError(cookies);
+  displayCookie(cookies);
 }
